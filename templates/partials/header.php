f51e80c249a659057bd82f052860cc83a6874bf1
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-31 18:49:09 +0100

    Update header.php

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,9 +1,18 @@
+<!-- Sudath Nawagamuwage | CO551-Open Source System | CW2-Student Course Administration System-->
+<!-- heade file for my page -->
+<!DOCTYPE html>
 <html>
-
-   <head>
-      <title>BNU Student Web Application</title>
-      <!-- styles and JS here -->
-
-   </head>
-
-   <body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css"/>
+        <title>CBNU Student Web Application</title>	
+        <!-- styles and JS here --> 
+    </head>
+    <body>
+        <div class="page-container">
+            <header>
+                <h1><h1>Student Course Administration System</h1></h1>
+            </header>
+            <div class="content-wrap">
+                <div class="container">
