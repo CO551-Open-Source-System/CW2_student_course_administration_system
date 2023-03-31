93d62f35d78775dbe1754058c388332087743153
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-31 20:54:14 +0100

    Update modules.php

--- modules.php
+++ modules.php
@@ -17,9 +17,13 @@
       $result = mysqli_query($conn,$sql);
 
       // prepare page content
-      $data['content'] .= "<table border='1'>";
-      $data['content'] .= "<tr><th colspan='5' align='center'>Modules</th></tr>";
+      // $data['content'] .= "<table border='1'>";
+      // $data['content'] .= "<tr><th colspan='5' align='center'>Modules</th></tr>";
+      // $data['content'] .= "<tr><th>Code</th><th>Type</th><th>Level</th></tr>";
+      $data['content'] .= "<table class='module-table'>";
+      $data['content'] .= "<tr><th colspan='3'>Modules</th></tr>";
       $data['content'] .= "<tr><th>Code</th><th>Type</th><th>Level</th></tr>";
+      
       // Display the modules within the html table
       while($row = mysqli_fetch_array($result)) {
          $data['content'] .= "<tr><td> $row[modulecode] </td><td> $row[name] </td>";
