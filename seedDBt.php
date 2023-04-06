ff3efec4bbfef81d358e87429eeac792cfb1ba13
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-04-06 11:32:44 +0100

    created the seed the data base

---
+++ seedDBt.php
@@ -0,0 +1,46 @@
+<?php
+
+// Load the Faker library
+require_once 'vendor/autoload.php';
+
+// Include configuration and database connection files
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+
+// Create a new Faker generator 
+$faker = Faker\Factory::create();
+
+// Loop 5 times to insert 5 fake student records into the database
+for ($i = 0; $i < 5; $i++) {
+
+    // Generate fake data for each field
+    $studentid = $faker->regexify('[A-Z]{2}\d{6}');
+    $password = password_hash($faker->password, PASSWORD_DEFAULT);
+    $dob = $faker->dateTimeBetween('-25 years', '-18 years')->format('Y-m-d');
+    $firstname = $faker->firstName;
+    $lastname = $faker->lastName;
+    $house = $faker->buildingNumber;
+    $town = $faker->city;
+    $county = $faker->state;
+    $country = $faker->country;
+    $postcode = $faker->postcode;
+    
+    // Construct SQL query to insert the fake data into the database
+    $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) 
+            VALUES ('$studentid', '$password', '$dob', '$firstname', '$lastname', '$house', '$town', '$county', '$country', '$postcode')";
+    
+    // Execute the SQL query
+    $result = mysqli_query($conn, $sql);
+    
+    // Check if there was an error inserting the record, and display the error message if there was
+    if (!$result) {
+        die("Error inserting record: " . mysqli_error($conn));
+    }
+}
+
+// Display a success message once all the records have been inserted
+echo "Records inserted successfully.";
+
+// Close the database connection
+mysqli_close($conn);
+?>
