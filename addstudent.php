1d189ab7aac174c74585b9723bc64aaee1ea50d1
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-04-06 13:03:41 +0100

    created the add student file

--- addstudent.php
+++ addstudent.php
@@ -1,80 +1,75 @@
 <?php
 
-// / Include configuration and database connection files
+// Include configuration and database connection files
 include("_includes/config.inc");
 include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
 
-
-// check if user is logged in
+// Check if user is logged in
 if (isset($_SESSION['id'])) {
 
-   // Include header and navigation templates
-   echo template("templates/partials/header.php");
-   echo template("templates/partials/nav.php");
+    // Include header and navigation templates
+    echo template("templates/partials/header.php");
+    echo template("templates/partials/nav.php");
 
-   // if the form has been submitted
-   if (isset($_POST['submit'])) {
+    // Check if the form has been submitted
+    if (isset($_POST['submit'])) {
 
-    //   // build an sql statment to update the student details
-    //   $sql = "update student set firstname ='" . $_POST['txtfirstname'] . "',";
-    //   $sql .= "lastname ='" . $_POST['txtlastname']  . "',";
-    //   $sql .= "house ='" . $_POST['txthouse']  . "',";
-    //   $sql .= "town ='" . $_POST['txttown']  . "',";
-    //   $sql .= "county ='" . $_POST['txtcounty']  . "',";
-    //   $sql .= "country ='" . $_POST['txtcountry']  . "',";
-    //   $sql .= "postcode ='" . $_POST['txtpostcode']  . "' ";
-    //   $sql .= "where studentid = '" . $_SESSION['id'] . "';";
-    //   $result = mysqli_query($conn,$sql);
+        // Hash the password
+        $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    //insert the statment 
-    
-    // Execute the SQL query
-    $result = mysqli_query($conn,$sql);
+        // Insert the new student record into the database
+        $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
+         VALUES ('$_POST[studentid]', '$hashed_password', '$_POST[dob]', '$_POST[firstname]', '$_POST[lastname]',
+          '$_POST[house]', '$_POST[town]', '$_POST[county]', '$_POST[country]', '$_POST[postcode]')";
 
-    $data['content'] = "<p>student recoard has been added</p>";
+        // Execute the SQL query
+        if (mysqli_query($conn, $sql)) {
+            $data['content'] = "<p>New student record has been added.</p>";
+        } else {
+            $data['content'] = "<p>Error: " . mysqli_error($conn) . "</p>";
+        }
 
-   }
-   else {
+    } else {
 
-
-      // using <<<EOD notation to allow building of a multi-line string
-      // see http://stackoverflow.com/questions/6924193/what-is-the-use-of-eod-in-php for info
-      // also http://stackoverflow.com/questions/8280360/formatting-an-array-value-inside-a-heredoc
-      $data['content'] = <<<EOD
-
-   <h2>Add New Studenet</h2>
-   <form name="frmdetails" action="" method="post">
-   //  student id  
-   First Name :
-   <input name="txtfirstname" type="text" value="" /><br/>
-   Surname :
-   <input name="txtlastname" type="text"  value="" /><br/>
-   //password
-   Number and Street :
-   <input name="txthouse" type="text"  value="" /><br/>
-   Town :
-   <input name="txttown" type="text"  value="" /><br/>
-   County :
-   <input name="txtcounty" type="text"  value="" /><br/>
-   Country :
-   <input name="txtcountry" type="text"  value="" /><br/>
-   Postcode :
-   <input name="txtpostcode" type="text"  value="" /><br/>
-   <input type="submit" value="Save" name="submit"/>
-   </form>
-
+        // Render the add student form
+        $data['content'] = <<<EOD
+        <h2>Add New Student</h2>
+        <form name="frmdetails" action="" method="post">
+            Student ID:
+            <input name="studentid" type="text" value="" required /><br/>
+            Password:
+            <input name="password" type="password" value="" required /><br/>
+            Date of Birth:
+            <input name="dob" type="date" value="" /><br/>
+            First Name:
+            <input name="firstname" type="text" value="" required /><br/>
+            Last Name:
+            <input name="lastname" type="text" value="" required /><br/>
+            Address:
+            <input name="house" type="text" value="" /><br/>
+            Town:
+            <input name="town" type="text" value="" /><br/>
+            County:
+            <input name="county" type="text" value="" /><br/>
+            Country:
+            <input name="country" type="text" value="" /><br/>
+            Postcode:
+            <input name="postcode" type="text" value="" /><br/>
+            <input type="submit" value="Save" name="submit"/>
+        </form>
 EOD;
+    }
 
-   }
-
-   // render the template
-   echo template("templates/default.php", $data);
+    // Render the template
+    echo template("templates/default.php", $data);
 
 } else {
-   header("Location: index.php");
+    // Redirect to login page if user is not logged in
+    header("Location: index.php");
 }
 
+// Include footer template
 echo template("templates/partials/footer.php");
 
 ?>
